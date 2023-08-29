e68643daeab50e847364991e4948f8670af06203
Casper Karlsen <casperbkarlsen@gmail.com>
2023-08-29 22:24:47 +0200

    Create bot.py
    Functional bot which can be used in discord

---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import random
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = True
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def rps(ctx, choice: str):
+    valid_choices = ['rock', 'paper', 'scissors']
+    bot_choice = random.choice(valid_choices)
+
+    if choice.lower() not in valid_choices:
+        await ctx.send("Invalid choice. Please choose 'rock', 'paper', or 'scissors'.")
+        return
+
+    result = determine_winner(choice.lower(), bot_choice)
+    await ctx.send(f"Bot chose {bot_choice}. {result}!")
+
+def determine_winner(player_choice, bot_choice):
+    if player_choice == bot_choice:
+        return "It's a tie"
+    elif (player_choice == 'rock' and bot_choice == 'scissors') or \
+         (player_choice == 'scissors' and bot_choice == 'paper') or \
+         (player_choice == 'paper' and bot_choice == 'rock'):
+        return "You win"
+    else:
+        return "Bot wins"
+
+@bot.command()
+async def coinflip(ctx):
+    result = random.choice(["Heads", "Tails"])
+    await ctx.send(f"The coin landed on {result}!")
+
+@bot.command()
+async def linda(ctx):
+    await ctx.send("Linda er deilig")
+
+
+bot.run('MTE0NjEyOTAzMDk1ODAzOTA3MA.G6kzHJ.4-RiMv97b2AUnU8KJQwLQKHofSxoGQ41ktEYg8')
